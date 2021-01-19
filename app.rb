c8431d775a3fd342814944014cb2d00316740e0b
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-01-19 20:01:54 +0000

    commit tree 4305c194025e3ae4eeb2053d327e5faa96b95750

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+      get '/name' do
+        "My name is Betty"
+      end 
+      
+      get '/hometown' do
+        "My hometown is London"
+      end 
+
+      get '/favorite-song' do
+        "My favorite song is Could you be loved by Bob Marley"
+      end 
+
 end
